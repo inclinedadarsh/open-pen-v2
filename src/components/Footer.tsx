51b3e428a7acfecf4cb1a29ff089bc9730b1625f
Adarsh Dubey <dubeyadarshmain@gmail.com>
2024-01-14 21:56:52 +0530

    feat: add footer component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,20 @@
+import Link from "next/link";
+
 const Footer = () => {
-    return <div>Footer</div>;
+    return (
+        <footer className='px-10 border-t text-center py-5'>
+            Copyright &copy; 2024,{" "}
+            <Link
+                href='https://adarshdubey.com/'
+                className='underline hover:no-underline'
+                target='_blank'
+                rel='noopener noreferrer'
+            >
+                Adarsh Dubey
+            </Link>
+            . All rights reserved.
+        </footer>
+    );
 };
 
 export default Footer;
