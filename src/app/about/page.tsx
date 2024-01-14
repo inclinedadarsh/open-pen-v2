7ce47ae4540d76dc67733c48493e10b0e68f29ca
Adarsh Dubey <dubeyadarshmain@gmail.com>
2024-01-14 21:49:54 +0530

    feat: add home page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -3,12 +3,14 @@ import Link from "next/link";
 const About = () => {
     return (
         <div className='flex justify-center items-center view-container'>
-            I added this for the sake of having another page. Please return to
-            the&nbsp;
-            <Link href='/' className='underline hover:no-underline'>
-                Home page
-            </Link>
-            .
+            <p>
+                I added this for the sake of having another page. Please return
+                to the&nbsp;
+                <Link href='/' className='underline hover:no-underline'>
+                    Home page
+                </Link>
+                .
+            </p>
         </div>
     );
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,32 @@
+import { buttonVariants } from "@/components/ui/button";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
-    return <h1>Hello world!</h1>;
+    return (
+        <main className='view-container grow mt-16 space-y-10'>
+            <h1 className='text-4xl lg:text-5xl font-semibold flex flex-col gap-2 md:gap-5'>
+                <span>Open Pen:</span>
+                <span>Simplify, Express, Share.</span>
+            </h1>
+            <p className='max-w-4xl'>
+                Streamline your blogging experience with our minimalist platform
+                designed for easy content creation and seamless sharing. Focus
+                on what matters most - your ideas - while enjoying a
+                user-friendly interface that lets your creativity shine. Join us
+                today and discover the joy of effortless blogging.
+            </p>
+            <div className='flex gap-5'>
+                <Link className={buttonVariants()} href='/login'>
+                    Login
+                </Link>
+                <Link
+                    className={buttonVariants({ variant: "secondary" })}
+                    href='/about'
+                >
+                    About us
+                </Link>
+            </div>
+        </main>
+    );
 }
